5a30c29a35147a9fddead5ffd065de1b99966e7a
P541M <videna.psalmeleazar@gmail.com>
2024-07-13 19:05:03 -0400

    feats: adjusted footer padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-bg py-10 px-20 text-text flex justify-center items-center">
+    <footer className="bg-bg pb-10 text-text flex justify-center items-center">
       <div className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-lg fade-up">
         &copy; {new Date().getFullYear()} Psalm Eleazar G. Videna.
       </div>
