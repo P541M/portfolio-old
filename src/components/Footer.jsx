b8d19093d9faa50c4a24280253f1e353f9c9b586
P541M <videna.psalmeleazar@gmail.com>
2024-07-14 21:20:48 -0400

    style: minor footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-bg pb-10 text-text flex justify-center items-center">
       <div className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-lg fade-up">
-        &copy; {new Date().getFullYear()} Psalm Eleazar G. Videna.
+        &copy; {new Date().getFullYear()} Psalm Eleazar G. Videna
       </div>
     </footer>
   );
