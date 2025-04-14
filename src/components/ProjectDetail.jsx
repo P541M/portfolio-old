bdcd5a5b6207cd92e4b1bd3705682f87b55cd924
P541M <videna.psalmeleazar@gmail.com>
2025-04-14 13:56:07 -0400

    adjusted the naming of the back to projects

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -377,7 +377,7 @@ const ProjectDetail = () => {
           className="mb-8 inline-flex items-center gap-2 text-primary hover:text-primary/80"
         >
           <FontAwesomeIcon icon={faArrowLeft} />
-          <span>Back to Projects</span>
+          <span>Back to Home</span>
         </Link>
 
         {/* Project header */}
